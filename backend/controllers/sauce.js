0add06dc2c46a37cf849d3eca1552273181e54a2
Gregoire De Fleurac <45977768+gregouz3@users.noreply.github.com>
2020-07-24 12:25:44 +0200

    mess error

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -61,6 +61,8 @@ exports.countLikeSauce = (req, res, next) => {
       $inc: {likes: 1} ,
       $push: {usersLiked: req.body.userId}
     })
+    .then (() => {res.status(201).json({ message: 'Sauce liked !'});})
+    .catch(error => res.status(400).json({ error }));
   } 
   if (req.body.like === -1) {
     Sauce.updateOne({ _id: req.params.id }, 
@@ -68,6 +70,8 @@ exports.countLikeSauce = (req, res, next) => {
       $inc: {dislikes: 1} ,
       $push: {usersDisliked: req.body.userId}
     })
+    .then (() => {res.status(201).json({ message: 'Sauce disliked !'});})
+    .catch(error => res.status(400).json({ error }));
   }
   if (req.body.like === 0) {
     Sauce.findOne({ _id: req.params.id})
@@ -78,6 +82,8 @@ exports.countLikeSauce = (req, res, next) => {
             $inc: {likes: -1},
             $pull: {usersLiked: req.body.userId},
           })
+          .then((sauce) => {res.status(201).json({ message: 'like -1'})}) 
+          .catch(error => res.status(400).json({ error }))
       } 
       if (sauce.usersDisliked.includes(req.body.userId)) {
         Sauce.updateOne({_id: req.params.id}, 
@@ -85,6 +91,8 @@ exports.countLikeSauce = (req, res, next) => {
             $inc: {dislikes: -1},
             $pull: {usersDisliked: req.body.userId}
           })
+          .then((sauce) => {res.status(201).json({ message: 'dislike -1'})}) 
+          .catch(error => res.status(400).json({ error }))
       }
     })
     .catch(error => res.status(400).json({ error }));
