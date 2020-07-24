1b012d869856eac48b3e33a83081b7ed67cc1938
Gregoire De Fleurac <45977768+gregouz3@users.noreply.github.com>
2020-07-24 11:02:35 +0200

    ordre middleware

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,8 +6,8 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config')
 
 router.get('/', sauceCtrl.getSauces);
-router.get('/:id', auth, sauceCtrl.getSauceById);  
 router.post('/', auth, multer, sauceCtrl.createSauce); 
+router.get('/:id', auth, sauceCtrl.getSauceById);  
 router.put('/:id', auth, multer, sauceCtrl.modifySauce); 
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth, sauceCtrl.countLikeSauce); 
